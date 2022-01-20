c2b8a4686d5b2bf8e34878a1c0f157549b75c892
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-01-20 07:27:44 -0300

    new commit test

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/notes/add",(req,res)=>{
+  res.render("notes/new-note")
+})
+
 router.get("/notes", (req, res) => {
-  res.send("notas desde la base de datos");
+  res.render("notas desde la base de datos");
 });
 
 module.exports = router;
